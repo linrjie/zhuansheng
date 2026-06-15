c99fbc83fbc89c99141c7b0910b1a2ced0d2725d
林润杰 <lrj327749624@hotmail.com>
2026-06-15 14:27:53 +0800

    使用JWT key作为默认配置

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ====== Supabase 配置 ======
-// 首次使用时在网页设置中填入，保存在浏览器 localStorage 中
-// 不会上传到 GitHub
+// 默认配置，首次打开自动连接
+// 可在网页 ⚙️ 设置中修改
 
-const DEFAULT_SUPABASE_URL = '';
-const DEFAULT_SUPABASE_KEY = '';
+const DEFAULT_SUPABASE_URL = 'https://ameledhpkrruwfbekkhz.supabase.co';
+const DEFAULT_SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtZWxlZGhwa3JydXdmYmVra2h6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTQ4NDY5NCwiZXhwIjoyMDk3MDYwNjk0fQ.WkXuhnr7pzp3kThuSFqf1rPuLVOSd_Ad8eiCQuqRXd8';
 const USER_ID = 'default_user';
