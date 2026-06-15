b15ab04384e58bea4c601408a18dd148ef5b9439
林润杰 <lrj327749624@hotmail.com>
2026-06-15 15:01:52 +0800

    切换为 anon key，浏览器可安全使用

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 // ====== Supabase 配置 ======
-// 默认配置，首次打开自动连接
-// 可在网页 ⚙️ 设置中修改
-
+// anon public key，可在浏览器安全使用
 const DEFAULT_SUPABASE_URL = 'https://ameledhpkrruwfbekkhz.supabase.co';
-const DEFAULT_SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtZWxlZGhwa3JydXdmYmVra2h6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTQ4NDY5NCwiZXhwIjoyMDk3MDYwNjk0fQ.WkXuhnr7pzp3kThuSFqf1rPuLVOSd_Ad8eiCQuqRXd8';
+const DEFAULT_SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtZWxlZGhwa3JydXdmYmVra2h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0ODQ2OTQsImV4cCI6MjA5NzA2MDY5NH0.cAgWCz_-r5eoFkX_Qzq5E-3lNN-vHKslimwbTvupnyk';
 const USER_ID = 'default_user';
